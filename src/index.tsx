43739dcf1e8389d976ab40ae06707e4ac5193abe
ren-joey <tsuyiren@gmail.com>
2022-12-13 05:45:39 +0800

    dev: initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import "./styles/tailwind.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import ReactDOM from 'react-dom/client';
+import './styles/tailwind.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = document.getElementById('root');
+
+if (root) {
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
