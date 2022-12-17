203c134248ac3470261e916bc7ebbe442bd17f0d
ren-joey <tsuyiren@gmail.com>
2022-12-18 02:29:10 +0800

    fix: merge conflict

--- src/data-set/data-factory.ts
+++ src/data-set/data-factory.ts
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env ts-node
-
-import getRandomTaiwanAddress from '../utils/string/get-random-taiwan-address';
-import { getRandomFamilyName, getRandomGivenName } from '../utils/string/get-random-chinese-name';
-import getRandomTaiwanId from '../utils/string/get-random-taiwan-id';
-import { PatientInfo } from './data-schema';
-
-const factory = () => {
-    const patientInfoArr: PatientInfo[] = [];
-    const currentDateNumber = Date.parse(new Date().toString());
-
-    for (let i = 0; i < 10; i++) {
-        patientInfoArr.push({
-            AcountID: getRandomTaiwanId(),
-            FamilyName: getRandomFamilyName(),
-            GivenName: getRandomGivenName(),
-            Sex: Math.random() > 0.5 ? 'M' : 'F',
-            Birthday: new Date(currentDateNumber - 500000000),
-            ChartNo: '0000001',
-            ContactAddress: getRandomTaiwanAddress(),
-            DeadDate: null,
-            RegisterAddress: getRandomTaiwanAddress()
-        });
-    }
-
-    console.log(patientInfoArr);
-};
-
-(factory)();
-=======
 import * as fs from 'fs';
 
 // const patientFactory = (): PatientObject => {
@@ -60,6 +29,5 @@ const dataFactory = () => {
 };
 
 (dataFactory)();
->>>>>>> 25a5939fd277e06ce6bdd6bf253deca9c05b27fc
 
 export {};
